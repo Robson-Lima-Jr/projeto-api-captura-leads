44b466c354223c883741747895bfcbae44405220
Rob Jr <robsonjuniorsn@gmail.com>
2025-12-02 18:02:59 -0300

    adiçao formulario

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Formulario from "./components/Formulario";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>hello world</h1>
+      <Formulario />
     </div>
   );
 }
